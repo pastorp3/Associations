6bd3bf2cf9afd4acff2666371133bd9b4c386b1f
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-10 12:32:00 -0500

    migrate association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_172509) do
+ActiveRecord::Schema.define(version: 2020_09_10_172739) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
