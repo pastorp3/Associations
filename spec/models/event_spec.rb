e36b6337eb59ddddd337b9de9e1d9f300df8588c
mateomh <mmojicah.84@gmail.com>
2020-09-11 23:56:26 -0500

    Create rspec tests for user and event models

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Event, :type => :model do
+  it 'Event gets the creator info' do
+    expect(Event.first.creator).not_to eq(nil)
+  end
+
+  it 'Event gets the attendances from the attendance model' do
+    expect(Event.first.attendances[0]).not_to eq(nil)
+  end
+
+  it 'Event get the user info through the attendances' do
+    expect(Event.first.attendances.first.user).not_to eq(nil)
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+  it 'User gets the attendances' do
+    expect(User.first.attendances[0]).not_to eq(nil)
+  end
+
+  it 'User gets the event info through attendances' do
+    expect(User.first.attendances.first.event).not_to eq(nil)
+  end
+
+  it 'User gets the info of its created events' do
+    expect(User.first.own_events[0]).not_to eq(nil)
+  end
+
+  it 'User gets the info of the attending events' do
+    expect(User.first.events[0]).not_to eq(nil)
+  end
+end
