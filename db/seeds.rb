28d2b622a1c439e52ad93fa933282ff67f97f54a
mateomh <mmojicah.84@gmail.com>
2020-09-12 18:27:51 -0500

    Add seeds for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ u = User.new
+ u.name = 'mateo'
+ u.email = 'mateo@test.com'
+ u.save
+ u.name = 'pastor'
+ u.email = 'pastor@test.com'
+ u.save
+ u.name = 'pancho'
+ u.email = 'pancho@test.com'
+ u.save
+
+ e = Event.new
+ e.title = 'test event 1'
+ e.body = '1 from the test database'
+ e.location = 'microverse'
+ e.date = '2020-09-10'
+ e.creator_id = 1
+ e.save
+ e.title = 'test event 2'
+ e.body = '2 from the test database'
+ e.location = 'microverse'
+ e.date = '2020-09-15'
+ e.creator_id = 2
+ e.save
+ 
+ a = Attendance.new
+ a.user_id = 1
+ a.event_id = 1
+ a.save
