45f8dc89f0ad0672259fecb623cb065f8d09f5c0
mateomh <mmojicah.84@gmail.com>
2020-09-12 19:12:30 -0500

    Add scope to the event model and update the controller and index view to use them

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,8 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @past = Event.all.past_events
+    @future = Event.all.upcoming_events 
     @user = User.find(session[:user_id])
   end
 
