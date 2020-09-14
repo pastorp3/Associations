30b213e1a25948f9fd2437a0ba0efe64037a88d7
mateomh <mmojicah.84@gmail.com>
2020-09-14 10:16:32 -0500

    Replace logic in the users and events helper by using the scope in the events model

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,13 +1,2 @@
 module EventsHelper
-  def event_clasify_events(events, direction)
-    results = []
-    events.each do |e|
-      if Date.today > e.date && direction == 'past'
-        results.push(e)
-      elsif Date.today <= e.date && direction == 'future'
-        results.push(e)
-      end
-    end
-    results
-  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,2 @@
 module UsersHelper
-  def user_clasify_events(events, direction)
-    results = []
-    events.each do |e|
-      if Date.today > e.event.date && direction == 'past'
-        results.push(e.event)
-      elsif Date.today <= e.event.date && direction == 'future'
-        results.push(e.event)
-      end
-    end
-    results
-  end
 end
